bafe96d75b8b25489a72c8e710cadf833f25c8fb
daikiojim <daiking.ca2@gmail.com>
2022-11-03 21:31:52 +0900

    fix: index.spec

--- src/utils/index.spec.ts
+++ src/utils/index.spec.ts
@@ -3,17 +3,13 @@ import { isJpyPair, toDisplayPair, formatFixed, toFixed } from './index'
 describe('toDisplayPair', () => {
   test('should get display pair', () => {
     expect(toDisplayPair('btc_jpy')).toBe('BTC/JPY')
-    expect(toDisplayPair('xrp_btc')).toBe('XRP/BTC')
+    expect(toDisplayPair('xrp_jpy')).toBe('XRP/JPY')
   })
 })
 
 describe('isJpyPair', () => {
   test('isJpyPair return true', () => {
     expect(isJpyPair('btc_jpy')).toBe(true)
-  })
-
-  test('isJpyPair return false', () => {
-    expect(isJpyPair('xrp_btc')).toBe(false)
   })
 })
 
