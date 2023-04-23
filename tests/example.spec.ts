3614073c21722ea20a2d181056f2803456066728
daikiojim <daiking.ca2@gmail.com>
2023-04-23 11:41:09 +0900

    feat: setup minimum test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,16 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('/bitbank-depth-chart/')
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await expect(page).toHaveTitle(/bitbank-depth-chart/)
+})
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('get header title', async ({ page }) => {
+  await page.goto('/bitbank-depth-chart/')
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  const header = page.locator('header')
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
+  await expect(header).toHaveText(/Depth Chart/)
+})
